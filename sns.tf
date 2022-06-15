2b4bebbf9107d1f15989ee01bfc3f9a46e410981
Phillip Langley <philliplangley@champmbplangley.lan>
2022-06-15 16:11:19 -0500

    adding cloudwatch alarm option

--- sns.tf
+++ sns.tf
@@ -1,10 +1,9 @@
 resource "aws_sns_topic" "this" {
-  count = var.enable_rds_metric_alarms ? 1 : 0
   name  = "${var.name}-alarms"
 }
 
 resource "aws_sns_topic_subscription" "this" {
-  count = var.enable_rds_metric_alarms ? 1 : 0
+  count = var.alarms_email != null ? 1 : 0
   depends_on = [
     aws_sns_topic.this
   ]
