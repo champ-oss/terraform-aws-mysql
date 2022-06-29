d35040096b3e38280176b4a62f00099e5e927005
Justin Reeves <reeves122@gmail.com>
2022-06-29 13:51:52 -0500

    remove snapshot checks from integration test

--- test/src/examples_complete_test.go
+++ test/src/examples_complete_test.go
@@ -5,13 +5,11 @@ import (
 	"github.com/gruntwork-io/terratest/modules/terraform"
 	"github.com/stretchr/testify/assert"
 	"os"
-	"os/exec"
 	"testing"
 	"time"
 )
 
-// TestExamplesComplete tests a typical deployment of this module. It will verify the SSM parameter is set correctly
-// and that snapshots are all working as expected.
+// TestExamplesComplete tests a typical deployment of this module
 func TestExamplesComplete(t *testing.T) {
 	t.Parallel()
 
@@ -23,34 +21,15 @@ func TestExamplesComplete(t *testing.T) {
 		},
 	}
 	defer terraform.Destroy(t, terraformOptions)
-
-	terraform.Init(t, terraformOptions)
-
-	// recursively set prevent destroy to false
-	cmd := exec.Command("bash", "-c", "find . -type f -name '*.tf' -exec sed -i'' -e 's/prevent_destroy = true/prevent_destroy = false/g' {} +")
-	cmd.Dir = "../../"
-	_ = cmd.Run()
-
-	terraform.ApplyAndIdempotent(t, terraformOptions)
+	terraform.InitAndApplyAndIdempotent(t, terraformOptions)
 
 	passwordSsmName := terraform.Output(t, terraformOptions, "password_ssm_name")
 	region := terraform.Output(t, terraformOptions, "region")
 	password := terraform.Output(t, terraformOptions, "password")
-	identifier := terraform.Output(t, terraformOptions, "identifier")
-	finalSnapshotIdentifier := terraform.Output(t, terraformOptions, "final_snapshot_identifier")
-	resourceId := terraform.Output(t, terraformOptions, "resource_id")
 	cloudwatchLogGroup := terraform.Output(t, terraformOptions, "lambda_cloudwatch_log_group")
 
 	logger.Log(t, "Creating AWS Session")
 	awsSess := GetAWSSession()
-
-	logger.Log(t, "Scheduling snapshots to be deleted")
-	defer DeleteDBBackup(awsSess, region, resourceId)
-	defer DeleteDBSnapshot(awsSess, region, finalSnapshotIdentifier)
-
-	logger.Log(t, "Running terraform plan to check for idempotency")
-	exitCode := terraform.PlanExitCode(t, terraformOptions)
-	assert.Equal(t, 0, exitCode)
 
 	logger.Log(t, "Validating the password is set correctly in SSM")
 	ssmParam := GetSSMParameter(awsSess, region, passwordSsmName)
@@ -58,17 +37,6 @@ func TestExamplesComplete(t *testing.T) {
 
 	logger.Log(t, "Validating the password is encrypted")
 	assert.Equal(t, "SecureString", *ssmParam.Type)
-
-	logger.Log(t, "Explicitly destroying the RDS so we can check the final snapshot")
-	terraform.Destroy(t, terraformOptions)
-
-	logger.Log(t, "Validating there is one automated/system snapshot (created when RDS is first created)")
-	automatedSnapshots := GetDBSnapshots(awsSess, region, identifier, "automated")
-	assert.Len(t, automatedSnapshots, 1)
-
-	logger.Log(t, "Validating there is one manual final snapshot (created when RDS is destroyed)")
-	manualSnapshots := GetDBSnapshots(awsSess, region, identifier, "manual")
-	assert.Len(t, manualSnapshots, 1)
 
 	logger.Log(t, "Waiting for IAM Auth Lambda to run")
 	time.Sleep(60 * time.Second)
--- test/src/util.go
+++ test/src/util.go
@@ -5,7 +5,6 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/cloudwatchlogs"
-	"github.com/aws/aws-sdk-go/service/rds"
 	"github.com/aws/aws-sdk-go/service/ssm"
 )
 
@@ -33,56 +32,6 @@ func GetSSMParameter(session *session.Session, region string, name string) *ssm.
 		panic(err)
 	}
 	return res.Parameter
-}
-
-// GetDBSnapshots returns a list of DB snapshots belonging to a specific RDS DB instance
-func GetDBSnapshots(session *session.Session, region string, dBInstanceIdentifier string, snapshotType string) []*rds.DBSnapshot {
-	fmt.Println("Getting DB Snapshots:", dBInstanceIdentifier)
-	svc := rds.New(session, aws.NewConfig().WithRegion(region))
-	res, err := svc.DescribeDBSnapshots(&rds.DescribeDBSnapshotsInput{
-		DBInstanceIdentifier: aws.String(dBInstanceIdentifier),
-		SnapshotType:         aws.String(snapshotType),
-	})
-	if err != nil {
-		panic(err)
-	}
-	return res.DBSnapshots
-}
-
-// DeleteDBSnapshot deletes a specific snapshot given a snapshot ID. It will wait to make sure the snapshot is
-// available before trying to delete it.
-func DeleteDBSnapshot(session *session.Session, region string, dBSnapshotIdentifier string) {
-	svc := rds.New(session, aws.NewConfig().WithRegion(region))
-
-	fmt.Println("Waiting for DB snapshot to become available:", dBSnapshotIdentifier)
-	err := svc.WaitUntilDBSnapshotAvailable(&rds.DescribeDBSnapshotsInput{
-		DBSnapshotIdentifier: aws.String(dBSnapshotIdentifier),
-	})
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println("Deleting DB Snapshot:", dBSnapshotIdentifier)
-	_, err = svc.DeleteDBSnapshot(&rds.DeleteDBSnapshotInput{
-		DBSnapshotIdentifier: aws.String(dBSnapshotIdentifier),
-	})
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("Successfully deleted DB Snapshot:", dBSnapshotIdentifier)
-}
-
-// DeleteDBBackup deletes a specific RDS backup given the DB resource ID
-func DeleteDBBackup(session *session.Session, region string, dbiResourceId string) {
-	fmt.Println("Deleting DB Backup:", dbiResourceId)
-	svc := rds.New(session, aws.NewConfig().WithRegion(region))
-	_, err := svc.DeleteDBInstanceAutomatedBackup(&rds.DeleteDBInstanceAutomatedBackupInput{
-		DbiResourceId: aws.String(dbiResourceId),
-	})
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("Successfully deleted DB Backup:", dbiResourceId)
 }
 
 func GetLogs(session *session.Session, region string, logGroup string, logStream *string) []*cloudwatchlogs.OutputLogEvent {
