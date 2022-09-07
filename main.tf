f9819500ffa54b1a9a3cef38564cf10ab898acea
Phillip Langley <philliplangley@champmbplangley.lan>
2022-09-07 14:21:51 -0500

    adding count

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ resource "random_password" "password" {
 
 # snapshot share must exist and have snapshot available, used for ephemeral testing
 data "aws_db_snapshot" "this" {
-  count = var.db_snapshot_source_arn != null ? 1 : 0
+  count                  = var.db_snapshot_source_arn != null ? 1 : 0
   db_snapshot_identifier = var.db_snapshot_source_arn
   snapshot_type          = "manual"
 }
