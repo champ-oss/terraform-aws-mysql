84a07efaf7e31f63c4642a29b4b51f591ac888ad
Justin Reeves <reeves122@gmail.com>
2022-11-04 15:05:09 -0500

    terraform fmt

--- main.tf
+++ main.tf
@@ -65,8 +65,8 @@ resource "aws_db_instance" "this" {
       identifier,
       identifier_prefix,
       final_snapshot_identifier,
-      db_name, # snapshots with a different name will show perpetual drift
-      username, # snapshots with a different username will show perpetual drift
+      db_name,        # snapshots with a different name will show perpetual drift
+      username,       # snapshots with a different username will show perpetual drift
       engine_version, # ignore drift for upgrades
       instance_class  # ignore instance class on upgrades
     ]
