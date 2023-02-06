c467e0fdae90de2fe85aa77a1b74376e2df0c8b5
Justin Reeves <reeves122@gmail.com>
2023-02-06 13:30:04 -0600

    change identifier

--- ssm.tf
+++ ssm.tf
@@ -4,11 +4,11 @@ resource "aws_ssm_parameter" "this" {
   type        = "SecureString"
   value       = random_password.password.result
   tags = merge({
-    master_username    = aws_db_instance.this.username
-    port               = aws_db_instance.this.port
-    endpoint           = aws_db_instance.this.endpoint
-    address            = aws_db_instance.this.address
-    cluster_identifier = aws_db_instance.this.identifier
+    master_username = aws_db_instance.this.username
+    port            = aws_db_instance.this.port
+    endpoint        = aws_db_instance.this.endpoint
+    address         = aws_db_instance.this.address
+    identifier      = aws_db_instance.this.identifier
   }, local.tags, var.tags)
 
   lifecycle {
